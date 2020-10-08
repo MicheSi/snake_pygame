75923fb6cf62158ae4bb398470fe92b8d6358f0c
mlazzell17@gmail.com <MicheSi>
2020-10-08 11:44:52 -0700

    game over if snake hits wall

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 
@@ -7,13 +8,16 @@ blue = (0, 0, 255)
 red = (255, 0, 0)
 
 # creates screen using pygame
-game_display = pygame.display.set_mode((600, 600))
-pygame.display.update()
+display_width = 600
+display_height = 600
+game_display = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Snake')
 
 # default start spot
-x1 = 300
-y1 = 300
+x1 = display_width/2
+y1 = display_height/2
+
+block = 10
 
 # coordinates on change
 x1_change = 0
@@ -23,6 +27,13 @@ y1_change = 0
 game_over = False
 
 clock = pygame.time.Clock()
+speed = 20
+
+font_style = pygame.font.SysFont(None, 50)
+
+def message(msg, color):
+    display_msg = font_style.render(msg, True, color)
+    game_display.blit(display_msg, [display_width/2, display_height/2])
 
 while not game_over:
     for event in pygame.event.get():
@@ -32,27 +43,34 @@ while not game_over:
         # key press events
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x1_change = -10
+                x1_change = -block
                 y1_change = 0
             elif event.key == pygame.K_RIGHT:
-                x1_change = 10
+                x1_change = block
                 y1_change = 0
             elif event.key == pygame.K_UP:
-                y1_change = -10
+                y1_change = -block
                 x1_change = 0
             elif event.key == pygame.K_DOWN:
-                y1_change = 10
+                y1_change = block
                 x1_change = 0
+
+    if x1 >= display_width or x1 < 0 or y1 >= display_height or y1 < 0:
+        game_over = True
 
     # move snake
     x1 += x1_change
     y1 += y1_change
 
     # draw rectangles
-    pygame.draw.rect(game_display, blue, [x1, y1, 10, 10])
+    pygame.draw.rect(game_display, blue, [x1, y1, block, block])
     pygame.display.update()
 
-    clock.tick(30)
+    clock.tick(speed)
     
+message('Game over', red)
+pygame.display.update()
+time.sleep(2)
+
 pygame.quit()
 quit()
