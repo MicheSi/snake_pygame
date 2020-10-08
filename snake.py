b85e4fc7b69bf2cd82a0498fd26e793b71e00fe6
mlazzell17@gmail.com <MicheSi>
2020-10-08 11:21:50 -0700

    added snake head

--- snake.py
+++ snake.py
@@ -7,6 +7,10 @@ game_display = pygame.display.set_mode((600, 600))
 pygame.display.update()
 pygame.display.set_caption('Snake')
 
+# initialize color variables
+blue = (0, 0, 255)
+red = (255, 0, 0)
+
 # creates loop to run game
 game_over = False
 while not game_over:
@@ -14,6 +18,10 @@ while not game_over:
         # allows user to close game
         if event.type == pygame.QUIT:
             game_over = True
+
+    # draw rectangles
+    pygame.draw.rect(game_display, blue, [200, 150, 10, 10])
+    pygame.display.update()
 
 pygame.quit()
 quit()
